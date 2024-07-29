1804010f702047927b9804fdc481949ed984ced4
Iván Villagrasa Pérez <56278092+ivvil@users.noreply.github.com>
2024-07-29 17:59:38 +0200

    Helper function for launching a server

--- minecraft-watchdog.lisp
+++ minecraft-watchdog.lisp
@@ -5,3 +5,9 @@
 (defmacro minecraft-invocation (java-path minecraft-jar &optional options)
   `(list ,java-path "-jar" ,minecraft-jar ,(or options "")))
 
+(defun launch-minecraft (java-path minecraft-jar &optional options)
+  (uiop:launch-program (minecraft-invocation java-path minecraft-jar options)
+					   :output :stream
+					   :input :stream))
+
+
